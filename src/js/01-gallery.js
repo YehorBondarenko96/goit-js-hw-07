23ee2008336385fe1762f0aa9535ee16c544e750
YehorBondarenko96 <egorbondarenko1996@gmail.com>
2023-09-19 18:34:55 +0300

    1.1

---
+++ src/js/01-gallery.js
@@ -0,0 +1,21 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+const gallery = document.querySelector('.gallery');
+const list = [];
+galleryItems.forEach((galleryItem) => {
+    const item = `<li class="gallery__item">
+    <a class="gallery__link" href="large-image.jpg">
+    <img
+        class="gallery__image"
+        src="${galleryItem.preview}"
+        data-source="${galleryItem.original}"
+        alt="${galleryItem.description}"
+    />
+    </a>
+</li>`
+    list.push(item);
+});
+
+gallery.insertAdjacentHTML('afterbegin', list.join(''));
+
+console.log(galleryItems);
