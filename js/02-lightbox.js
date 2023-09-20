9bd2151cb2dd477738bad4501cec15d32dc982ff
YehorBondarenko96 <egorbondarenko1996@gmail.com>
2023-09-20 17:38:13 +0300

    2.1

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const gallery = document.querySelector('.gallery');
+const list = [];
+galleryItems.forEach((galleryItem) => {
+    const item = `<li class="gallery__item">
+    <a class="gallery__link" href="${galleryItem.original}">
+        <img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}" />
+    </a>
+</li>`
+    list.push(item);
+});
+
+gallery.insertAdjacentHTML('afterbegin', list.join(''));
+
 console.log(galleryItems);
+
+const options = {
+    captions: true,
+    captionSelector: 'img',
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+    close: false, 
+};
+var lightbox = new SimpleLightbox('.gallery a',  options );
